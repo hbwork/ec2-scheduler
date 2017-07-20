03b9ee10fe01b5ea168eaa96ce73282aa9ee33bb
Eric Ho <eric.ho@datacom.com.au>
2017-07-20 17:26:20 +1000

    HCS-760 New featre - RDS instance support

--- code/ec2-scheduler.py
+++ code/ec2-scheduler.py
@@ -25,19 +25,6 @@ import re
 # Import pytz to support local timezone
 import pytz
 
-# Global variables definition
-# Weekdays Interpreter
-weekdays = ['mon', 'tue', 'wed', 'thu', 'fri']
-
-# Monthdays Interpreter (1..31)
-monthdays = re.compile(r'^(0?[1-9]|[12]\d|3[01])$')
-
-# nth weekdays Interpreter
-nthweekdays=re.compile('\w{3}/\d{1}')
-
-# Split out Tag & Set Variables to default
-default1 = 'default'
-default2 = 'true'
 
 #Default startTime, stopTime, TimeZone
 defaultStartTime = 'none'
@@ -67,6 +54,19 @@ def putCloudWatchMetric(region, instance_id, instance_state):
     )
 
 def scheduler_action(tagValue):
+    # Weekdays Interpreter
+    weekdays = ['mon', 'tue', 'wed', 'thu', 'fri']
+
+    # Monthdays Interpreter (1..31)
+    monthdays = re.compile(r'^(0?[1-9]|[12]\d|3[01])$')
+
+    # nth weekdays Interpreter
+    nthweekdays=re.compile('\w{3}/\d{1}')
+
+    # Split out Tag & Set Variables to default
+    default1 = 'default'
+    default2 = 'true'
+
     #Default scheduler action
     Action = 'None'
 
@@ -90,6 +90,11 @@ def scheduler_action(tagValue):
         else:
             startTime = ptag[0]
             stopTime = ptag[0]
+
+    # Support 24x7
+    if startTime  == '24x7':
+        return "START"
+
     if len(ptag) >= 2:
         stopTime = ptag[1]
     if len(ptag) >= 3:
@@ -115,16 +120,11 @@ def scheduler_action(tagValue):
 
     now = datetime.datetime.now(tz).strftime("%H%M")
 
-    # Support 24x7
-    if startTime  == '24x7':
-        tartTime = now
-
     if  datetime.datetime.now(tz).strftime("%H") != '00':
         nowMax = datetime.datetime.now(tz) - datetime.timedelta(minutes=59)
         nowMax = nowMax.strftime("%H%M")
     else:
         nowMax = "0000"
-        daysActive
 
     nowDay = datetime.datetime.now(tz).strftime("%a").lower()
     nowDate = int(datetime.datetime.now(tz).strftime("%d"))
@@ -136,35 +136,38 @@ def scheduler_action(tagValue):
     elif daysActive == "weekdays":
         if (nowDay in weekdays):
             isActiveDay = True
-        else:
-            for d in daysActive.split(","):
-                # mon, tue,wed,thu,fri,sat,sun ?
-                if d.lower() == nowDay:
+    else:
+        for d in daysActive.split(","):
+            # mon, tue,wed,thu,fri,sat,sun ?
+            if d.lower() == nowDay:
                     isActiveDay = True
-                # Month days?
-                elif monthdays.match(d):
-                    if int(d) == nowDate:
-                        isActiveDay = True
-                # mon/1 first Monday of the month
-                # tue/2 second Tuesday of the month
-                # Fri/3 third Friday of the month
-                # sat/4 forth Saturday of the month
-                elif nthweekdays.match(d):
-                    (weekday,nthweek) = d.split("/")
+            # Month days?
+            elif monthdays.match(d):
+                if int(d) == nowDate:
+                     isActiveDay = True
+            # mon/1 first Monday of the month
+            # tue/2 second Tuesday of the month
+            # Fri/3 third Friday of the month
+            # sat/4 forth Saturday of the month
+            elif nthweekdays.match(d):
+                (weekday,nthweek) = d.split("/")
 
-                    if (weekday.lower() == nowDay) and ( nowDate >= (int(nthweek) * 7 - 6)) and (nowDate <= (int(nthweek) * 7)):
-                        isActiveDay = True
-
+                if (weekday.lower() == nowDay) and ( nowDate >= (int(nthweek) * 7 - 6)) and (nowDate <= (int(nthweek) * 7)):
+                   isActiveDay = True
 
     if startTime >= str(nowMax) and startTime <= str(now) and isActiveDay == True and isValidTimeZone == True:
-        Action = "Start"
+        Action = "START"
 
     if stopTime >= str(nowMax) and stopTime <= str(now) and isActiveDay == True and isValidTimeZone == True:
-        Action = "Stop"
+        Action = "STOP"
 
     return Action
 
 def lambda_handler(event, context):
+    global defaultStartTime
+    global defaultStopTime
+    global defaultTimeZone
+    global defaultDaysActive
 
     print "Running EC2 and RDS Scheduler"
 
@@ -182,13 +185,15 @@ def lambda_handler(event, context):
     # Reading Default Values from CloudWatch Rule Input event
     customTagName = event['CustomTagName']
     customTagLen = len(customTagName)
+    sendData = event['SendAnonymousData'].lower()
+    createMetrics = event['CloudWatchMetrics'].lower()
+    UUID = event['UUID']
+
+    # Set global default value
     defaultStartTime = event['DefaultStartTime']
     defaultStopTime = event['DefaultStopTime']
     defaultTimeZone = event['DefaultTimeZone']
     defaultDaysActive = event['DefaultDaysActive']
-    sendData = event['SendAnonymousData'].lower()
-    createMetrics = event['CloudWatchMetrics'].lower()
-    UUID = event['UUID']
 
     #Customized RDS Tag Name
     RDSSupport = event['RDSSupport']
@@ -218,7 +223,7 @@ def lambda_handler(event, context):
             # List all instances
             instances = ec2.instances.all()
 
-            print "Creating ", region_name, " instance lists..."
+            print "Creating ", region_name, " EC2 instance lists..."
 
             for i in instances:
                 if i.tags != None:
@@ -238,7 +243,7 @@ def lambda_handler(event, context):
                             action = scheduler_action(tagValue = t['Value'])
 
                             # Append to start list
-                            if action == 'Start' and state == "stopped":
+                            if action == "START" and state == "stopped":
                                 if i.instance_id not in startList:
                                     startList.append(i.instance_id)
                                     print i.instance_id, " added to START list"
@@ -247,7 +252,7 @@ def lambda_handler(event, context):
                                 # Instance Id already in startList
 
                             # Append to stop list
-                            if action == 'Stop' and state == "running":
+                            if action == "STOP" and state == "running":
                                 if i.instance_id not in stopList:
                                     stopList.append(i.instance_id)
                                     print i.instance_id, " added to STOP list"
@@ -265,13 +270,13 @@ def lambda_handler(event, context):
                 print "Starting", len(startList), "instances", startList
                 ec2.instances.filter(InstanceIds=startList).start()
             else:
-                print "No Instances to Start"
+                print "No Instances to Start in region ",  region_name
 
             if stopList:
                 print "Stopping", len(stopList) ,"instances", stopList
                 ec2.instances.filter(InstanceIds=stopList).stop()
             else:
-                print "No Instances to Stop"
+                print "No Instances to Stop in region ", region_name
 
             # Built payload for each region
             if sendData == "yes":
@@ -307,25 +312,73 @@ def lambda_handler(event, context):
             continue
 
         if RDSSupport == "Yes":
+            # Declare Lists
+            rdsStartList = []
+            rdsStopList = []
+            rdsRunningStateList = []
+            rdsStoppedStateList = []
+
+            print "Creating ", region_name, " RDS instance lists..."
             try:
-                rds = boto3.client('rds', region_name =  'ap-southeast-2')
+                rds = boto3.client('rds', region_name =  region_name)
                 rds_instances = rds.describe_db_instances()
 
                 for rds_instance in rds_instances['DBInstances']:
-                    print "----------"
-                    print rds_instance
-                    print rds_instance['DBInstanceIdentifier']
-                    print rds_instance['MultiAZ']
-                    print rds_instance['ReadReplicaDBInstanceIdentifiers']
-                    print rds_instance['DBInstanceArn']
 
+                    if len(rds_instance['ReadReplicaDBInstanceIdentifiers']):
+                        print "----- No action against RDS instance (with read replica): ", rds_instance['DBInstanceIdentifier']
+                        continue
+
+                    if "ReadReplicaSourceDBInstanceIdentifier" in rds_instance.keys():
+                        print "----- No action against RDS instance (replication): ", rds_instance['DBInstanceIdentifier']
+                        continue
+
+                    if rds_instance['MultiAZ']:
+                        print "----- No action against RDS instance (Multiple AZ): ", rds_instance['DBInstanceIdentifier']
+                        continue
+
+                    if rds_instance['DBInstanceStatus'] not in ["available","stopped"]:
+                        print "----- No action against RDS instance (unsupported status):", rds_instance['DBInstanceIdentifier']
+                        continue
+
+                    # Query RDS instance tags 
                     response = rds.list_tags_for_resource( ResourceName = rds_instance['DBInstanceArn'])
                     tags = response['TagList']
 
-                    print tags
                     for t in tags:
                         if t['Key'][:customRDSTagLen] == customRDSTagName:
-                            print customRDSTagName, " : ", t['Value']
+
+                            action = scheduler_action(tagValue = t['Value'])
+                            state = rds_instance['DBInstanceStatus'] 
+
+                            # Append to start list
+                            if action == "START" and state == "stopped":
+                                if rds_instance['DBInstanceIdentifier'] not in rdsStartList:
+                                    rdsStartList.append(rds_instance['DBInstanceIdentifier'])
+                                    print rds_instance['DBInstanceIdentifier'], " added to RDS START list"
+                                # Instance Id already in rdsStartList
+
+                            # Append to stop list
+                            if action == "STOP" and state == "available":
+                                if rds_instance['DBInstanceIdentifier'] not in rdsStopList:
+                                    rdsStopList.append(rds_instance['DBInstanceIdentifier'])
+                                    print rds_instance['DBInstanceIdentifier'], " added to RDS STOP list"
+                                # Instance Id already in rdsStopList
+
+                # Execute Start and Stop Commands
+                if rdsStartList:
+                    print "Starting", len(rdsStartList), "RDS instances", rdsStartList
+                    for DBInstanceIdentifier in rdsStartList:
+                        rds.start_db_instance(DBInstanceIdentifier = DBInstanceIdentifier)
+                else:
+                    print "No RDS Instances to Start in region ",  region_name
+
+                if rdsStopList:
+                    print "Stopping", len(rdsStopList) ,"RDS instances", rdsStopList
+                    for DBInstanceIdentifier in rdsStopList:
+                        rds.stop_db_instance(DBInstanceIdentifier = DBInstanceIdentifier)
+                else:
+                    print "No RDS Instances to Stop in region ", region_name
 
             except Exception as e:
                 print ("Exception: "+str(e))
@@ -362,7 +415,7 @@ if  __name__ =='__main__':
         "SendAnonymousData": "No",
         "CustomTagName": "scheduler:ec2-startstop",
         "CustomRDSTagName": "scheduler:rds-startstop",
-        "CloudWatchMetric": "Enabled"
+        "CloudWatchMetrics": "Enabled"
     }
 
     context = None
